8f87f1a11c1996caabb309674a691d0cf47054dd
Ornelle <42754183+ornelle@users.noreply.github.com>
2021-02-09 15:26:44 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+## Hello World for P5.js
+#### by Ornelle Fonkoua
+### Taken from Template provided by @scottkildall
+February 9th, 2020
+
+
+### Overview
+Simple Hello World text display.
+
+### Technical Details
+
+Modify the sketch.js setup() and draw() are used only 
+Added colors to the hello text 
---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(800, 800);
+  textSize(100);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  noStroke();
+  fill('blue');
+  text("Hello World w P5!", width/2, height/2);
+  
+
+}
+
+// display effect 
+function mousePressed() {
+  clear();
+}
